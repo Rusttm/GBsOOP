62f0a8997720bb5300c6f0fd1cccd40e5abca73f
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-29 16:17:03 +0300

    lesson1 initial commit

--- lesson1/Main.java
+++ lesson1/Main.java
@@ -1,5 +1,8 @@
+import HumanClass
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World1");
+        HumanClass man = new HumanClass();
+
     }
 }
