75bdfcf1ed7a967561b4db8fef4e3e6963a46511
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-04-04 21:28:45 +0300

    cart interface comments commit

--- lesson1/src/main/java/seminar2/CartInterface.java
+++ lesson1/src/main/java/seminar2/CartInterface.java
@@ -1,6 +1,12 @@
 package seminar2;
 
+/*
+* Пока не рабочий интерфейс
+* */
 public interface CartInterface {
-
+    public double getCartSum();
+    public double getCartProdNum();
+    public double getCartVatSum();
+    public int getNumPos();
 
 }
