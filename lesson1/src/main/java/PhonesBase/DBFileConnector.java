363b5f234b21af2cffa5c9eaaaf14d0ca0505b85
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-04-26 04:36:16 +0300

    db connector filter fin commit

--- lesson1/src/main/java/PhonesBase/DBFileConnector.java
+++ lesson1/src/main/java/PhonesBase/DBFileConnector.java
@@ -3,6 +3,7 @@ package PhonesBase;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 
@@ -10,7 +11,7 @@ import java.util.Scanner; // Import the Scanner class to read text files
  * Коннектор к файлу с телефонами
  */
 public class DBFileConnector implements IDBconnector {
-    public ArrayList<HashMap> dataBase;
+    private ArrayList<HashMap> dataBase;
     private String fileName;
     public static void main(String[] args) {
 //        Тестовые наполнения:
@@ -19,14 +20,17 @@ public class DBFileConnector implements IDBconnector {
 //        ArrayList testDataBase = DBFullFillment();
         DBFileConnector newConnect = new DBFileConnector("PhoneDB");
         newConnect.DBFullFillment();
-        HashMap<String, String> prod4 = new HashMap<>();
-        prod4.put("id", "4");
-        prod4.put("name", "Google");
-        System.out.println(newConnect.putProd2DB(prod4));
-        HashMap<String, String> prod5 = new HashMap<>();
-        prod5.put("id", "1");
-        prod5.put("name", "IPhon");
-        System.out.println(newConnect.putProd2DB(prod5));
+        HashMap<String,String> newFilter = new HashMap<>();
+        newFilter.put("memory","64");
+        newFilter.put("name","honor");
+        System.out.printf("По фильтру %s найдено ", newFilter.toString());
+        System.out.println(newConnect.getFilteredDB(newFilter));
+//        System.out.println(newConnect.dataBase);
+//        HashMap<String, String> prod5 = new HashMap<>();
+//        prod5.put("id", "1");
+//        prod5.put("name", "IPhone");
+//        newConnect.updProd2DB(prod5);
+
 
 
 //        System.out.println(newConnect.dataBase);
@@ -131,15 +135,24 @@ public class DBFileConnector implements IDBconnector {
         HashMap<String, String> prod1 = new HashMap<>();
         prod1.put("id", "1");
         prod1.put("name", "IPhone");
+        prod1.put("memory", "64");
         HashMap<String, String> prod2 = new HashMap<>();
         prod2.put("id", "2");
         prod2.put("name", "Xiaomi");
+        prod2.put("memory", "128");
         HashMap<String, String> prod3 = new HashMap<>();
         prod3.put("id", "3");
         prod3.put("name", "Honor");
+        prod3.put("memory", "64");
         dataBase.add(prod1);
         dataBase.add(prod2);
         dataBase.add(prod3);
+        HashMap<String, String> prod4 = new HashMap<>();
+        prod4.put("id", "4");
+        prod4.put("name", "Google");
+        prod4.put("memory", "32");
+        dataBase.add(prod4);
+
         this.dataBase = dataBase;
         this.write2FileFromArray(dataBase);
 
@@ -147,8 +160,43 @@ public class DBFileConnector implements IDBconnector {
 
 
     @Override
-    public ArrayList getFilteredDB() {
-        return null;
+    public ArrayList getFilteredDB(HashMap<String, String> filterMap) {
+        ArrayList resultArray = new ArrayList<>();
+//        ArrayList resultArray = (ArrayList) this.dataBase.clone();
+//        Ищем по ключу
+        HashSet<Integer> removeIndexes = new HashSet<>();
+        for (String filterKey: filterMap.keySet()) {
+            String filterValue = filterMap.get(filterKey);
+            for (int i = 0; i < this.dataBase.size(); i++) {
+                HashMap<String,String> prod = (HashMap) this.dataBase.get(i);
+                try {
+                    String value = prod.get(filterKey);
+//                    Исключаем, если не подходит
+                    if ( value.toLowerCase().compareTo(filterValue.toLowerCase()) !=0 ) {
+                        removeIndexes.add(i);
+                    }
+
+                } catch (Exception e) {
+//                    System.out.printf("В продукте %s не найдено значение %s",prod.toString(), filterKey);
+                    removeIndexes.add(i);
+//                    throw new RuntimeException(e);
+                }
+
+            }
+//            System.out.println(removeIndexes);
+
+        }
+        for (int i = 0; i < this.dataBase.size(); i++) {
+//            System.out.println(removeIndexes.contains(i));
+            if (!removeIndexes.contains(i)) {
+//                    System.out.println(removeIndexes.contains(i));
+                    resultArray.add(this.dataBase.get(i));
+            }
+
+        }
+
+
+        return resultArray;
     }
 
     @Override
@@ -163,6 +211,9 @@ public class DBFileConnector implements IDBconnector {
         return null;
     }
 
+    /**Выдать всю базу
+     * @return возвращает ArrayList
+     */
     @Override
     public ArrayList getAllDb() {
         return this.dataBase;
@@ -176,10 +227,13 @@ public class DBFileConnector implements IDBconnector {
     @Override
     public boolean putProd2DB(HashMap<String, String> newProd) {
         String newValue = newProd.get("id");
+        // Сначала проверить есть ли в базе?
         HashMap isNotNew = this.getProductByID(newValue);
         if (isNotNew==null) {
             this.dataBase.add(newProd);
             this.write2FileFromArray(this.dataBase);
+            System.out.printf("Продукт %s добавлен", newProd.toString());
+            System.out.println();
             return true;
         }
         System.out.println("Продукт с данным id уже существует");
@@ -197,14 +251,34 @@ public class DBFileConnector implements IDBconnector {
                 workArray.remove(i);
                 this.write2FileFromArray(workArray);
                 this.readFromFile2Array();
+                System.out.printf("Продукт %s удален", prod.toString());
+                System.out.println();
                 return true;
             }
         }
+        System.out.println("Продукт с данным ID не удален или не существует в Базе");
         return false;
     }
 
+
     @Override
-    public boolean updProd2DB() {
+    public boolean updProd2DB(HashMap<String, String> updProd) {
+        String updValue = updProd.get("id");
+        // Сначала проверить есть ли в базе?
+        HashMap isNotNew = this.getProductByID(updValue);
+        if (isNotNew!=null) {
+            this.dataBase.remove(isNotNew);
+            this.dataBase.add(updProd);
+            this.write2FileFromArray(this.dataBase);
+            System.out.printf("Продукт %s обновлен на %s", isNotNew.toString(),updProd.toString());
+            System.out.println();
+            return true;
+        }
+        System.out.println("Продукт не найден в базе");
         return false;
+    }
+
+    public ArrayList<HashMap> getDataBase() {
+        return dataBase;
     }
 }
--- lesson1/src/main/java/PhonesBase/IDBconnector.java
+++ lesson1/src/main/java/PhonesBase/IDBconnector.java
@@ -6,27 +6,48 @@ import java.util.HashMap;
 /** Интерфейс работы с Базой данных
  **/
 
-
+/**Коннектор базы данных по телефонам(любым продуктам)
+ * должен получать данные из файла или БД и выдавать из по запросу
+ * @ getFilteredDB() выдает данные по фильтрам
+ * @ getProductByID() выдает продукт по ID
+ * @ getAllDb() выдает всю базу
+ * @ putProd2DB() положить продукт в БД
+ * @ delProdFromDB() удалить продукт из БД
+ * @ updProd2DB() изменить продукт в БД
+ * */
 public interface IDBconnector {
-    /**Коннектор базы данных по телефонам(любым продуктам)
-     * должен получать данные из файла или БД и выдавать из по запросу
-     * @ getFilteredDB() выдает данные по фильтрам
-     * @ getProductByID() выдает продукт по ID
-     * @ getAllDb() выдает всю базу
-     * @ putProd2DB() положить продукт в БД
-     * @ delProdFromDB() удалить продукт из БД
-     * @ updProd2DB() изменить продукт в БД
-     * */
-    ArrayList getFilteredDB();
 
+
+    /** Метод выдает продукты которые подходят по фильтрам
+     * @param filterMap получает на входе HashMap {фильтр:значение}
+     * @return возвращает ArrayList с продуктами, либо пустой
+     */
+    ArrayList getFilteredDB(HashMap<String, String> filterMap);
+
+    /** Метод для выдачи продукта по id
+     * @param id искомого продукта
+     * @return возвращает сам продукт если он существует в базе, и null -если нет
+     */
     HashMap getProductByID(String id);
 
+
+    /** Метод выдает всю базу
+     * @return в виде ArrayList
+     */
     ArrayList getAllDb();
 
+    /** Метод для добавления продукта
+     * @param prod новые данные в виде HashMap
+     * @return возвращает true если добавил, и false -если нет
+     */
     boolean putProd2DB(HashMap<String, String> prod);
 
     boolean delProdFromDB(String id);
 
-    boolean updProd2DB();
 
+    /** Метод для обновления продукта
+     * @param updProd новые данные в виде HashMap
+     * @return возвращает true если обновил, и false -если не обновил
+     */
+    boolean updProd2DB(HashMap<String, String> updProd);
 }
